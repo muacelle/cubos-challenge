f966cfbd5bc587758ea6667ef68a003dabfbdd8f
Marcelle Pernet <mevpernet@gmail.com>
2022-11-12 16:43:54 -0300

    modal css

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -75,7 +75,15 @@ const Modal = () => {
                 </section>
 
                 <section className='modal-video'>
-                    <iframe width="560" height="315" src={selectedMovie?.trailer} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <hr/>
+                    <iframe 
+                    width="1000" 
+                    height="500" 
+                    src={selectedMovie?.trailer} 
+                    title="YouTube video player" 
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullScreen></iframe>
                 </section>
 
             </section>
