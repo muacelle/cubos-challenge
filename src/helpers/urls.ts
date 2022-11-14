691f7b039fdb30d77c833ae85b65c8ce569129e8
Marcelle Pernet <mevpernet@gmail.com>
2022-11-14 17:28:01 -0300

    urls deleted

--- src/helpers/urls.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    IMAGE_BASE: 'https://image.tmdb.org/t/p/w200',
-    IMAGE_BASE_MODAL: 'https://image.tmdb.org/t/p/w300',
-    SEARCH: '/search',
-    DISCOVER: '/discover',
-    GENRES: '&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres='
-}
